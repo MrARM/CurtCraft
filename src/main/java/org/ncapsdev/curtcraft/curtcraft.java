e28e2693adf87d6f37bbdf3e29524af249f41492
MrARM <jordan32710@gmail.com>
2017-10-13 20:51:10 -0500

    Make curt spawnable

--- src/main/java/org/ncapsdev/curtcraft/curtcraft.java
+++ src/main/java/org/ncapsdev/curtcraft/curtcraft.java
@@ -9,6 +9,8 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.EntityRegistry;
+import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.world.biome.BiomeGenBase;
 
 @Mod(modid="curtcraft", version="1.0", name="CurtCraft", acceptedMinecraftVersions="[1.7.10]")
 public class curtcraft
@@ -23,6 +25,9 @@ public void preInit(FMLPreInitializationEvent e)
 	CurtDisc.mainRegistry();
 	//The Curt Himself
 	 EntityRegistry.registerModEntity(EntityCurt.class, "Curt_Kelly", 420, this, 50, 3, true);
+	 //EntityRegistry.addSpawn("Curt_Kelly", 32, 1, 4, EnumCreatureType.monster, BiomeGenBase.getBiomeGenArray());
+	 //Curts asexual system
+	 CurtEgg.mainRegistry();
 }
 
 @EventHandler
