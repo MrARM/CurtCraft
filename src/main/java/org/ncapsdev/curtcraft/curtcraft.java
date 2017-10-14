1a40309e7c27890fdaaac21d0eeab50f9c1ec2b0
MrARM <jordan32710@gmail.com>
2017-10-13 20:16:00 -0500

    Add curt base entity

--- src/main/java/org/ncapsdev/curtcraft/curtcraft.java
+++ src/main/java/org/ncapsdev/curtcraft/curtcraft.java
@@ -1,10 +1,14 @@
 package org.ncapsdev.curtcraft;
 
+import org.ncapsdev.curtcraft.entities.EntityCurt;
+import org.ncapsdev.curtcraft.items.*;
+
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.EntityRegistry;
 
 @Mod(modid="curtcraft", version="1.0", name="CurtCraft", acceptedMinecraftVersions="[1.7.10]")
 public class curtcraft
@@ -15,7 +19,10 @@ public static String VERSION = "1.0";
 @EventHandler
 public void preInit(FMLPreInitializationEvent e)
 {
+	//Curt's Jamz
 	CurtDisc.mainRegistry();
+	//The Curt Himself
+	 EntityRegistry.registerModEntity(EntityCurt.class, "Curt_Kelly", 420, this, 50, 3, true);
 }
 
 @EventHandler
